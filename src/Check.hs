4fca3fe84ee72535f2e62b25f80ff6bdcf9adce3
alexis <alexis.auriac@epitech.eu>
2019-03-01 21:46:52 +0100

    Improved coding a bit

--- src/Check.hs
+++ src/Check.hs
@@ -9,15 +9,18 @@ getWords order line = getWords' line
     where
         getWords' :: String -> [String]
         getWords' line'
-            | length line' == 0 = []
-            | length line' < order = (line' ++ take (order - (length line')) line) : getWords' (drop 1 line')
+            | lenLine == 0 = []
+            | lenLine < order = 
+                (line' ++ take (order - lenLine) line) : getWords' (drop 1 line')
             | otherwise = take order line' : getWords' (drop 1 line')
+            where
+                lenLine = length line'
 
 check :: Int -> String -> IO ()
 check order alphabet = getLine >>= check'
     where
         check' line
-            | length line /= (length alphabet) ^ order = putStrLn "KO"
-            | not (allElemsOf line alphabet) = putStrLn "KO"
-            | not (allDifferent (getWords order line)) = putStrLn "KO"
+            | length line /= length alphabet ^ order = putStrLn "KO"
+            | not $ allElemsOf line alphabet = putStrLn "KO"
+            | not $ allDifferent $ getWords order line = putStrLn "KO"
             | otherwise = putStrLn "OK"

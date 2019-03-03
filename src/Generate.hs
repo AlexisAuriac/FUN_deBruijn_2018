8e61bd1cfe08e9aee4afddabbe396071d4081b5a
alexis <alexis.auriac@epitech.eu>
2019-03-03 14:58:03 +0100

    generation: finished prefer one algo

--- src/Generate.hs
+++ src/Generate.hs
@@ -32,23 +32,25 @@ import Data.Maybe
 
 tryAddSeq :: String -> [String] -> String -> Maybe String
 tryAddSeq alphabet done lastN =
-    if (lastN ++ "1") `elem` done
-        then if (lastN ++ "0") `elem` done
+    if (lastN ++ [alphabet !! 1]) `elem` done
+        then if (lastN ++ (take 1 alphabet)) `elem` done
             then Nothing
-            else Just "0"
-        else Just "1"
+            else Just (take 1 alphabet)
+        else Just [(alphabet !! 1)]
 
-db :: String -> [String] -> String
+db :: Int -> String -> String -> [String] -> String
 -- db :: String -> [String] -> [String]
-db dBSeq done = do
+db order alpha dBSeq done = do
     let lenSeq = length dBSeq
-    let lastN = drop (lenSeq - 2) dBSeq
-    let newLastChar = tryAddSeq "01" done lastN
+    let lastN = drop (lenSeq - order + 1) dBSeq
+    let newLastChar = tryAddSeq alpha done lastN
 
-    case newLastChar of
-        Nothing -> dBSeq
+    if length done == (length alpha) ^ order - 1
+        then dBSeq
+        else db order alpha (dBSeq ++ (fromJust newLastChar)) (done ++ [lastN ++ (fromJust newLastChar)])
+    -- case newLastChar of
+        -- Nothing -> dBSeq
         -- Nothing -> done
-        _ -> db (dBSeq ++ (fromJust newLastChar)) (done ++ [lastN ++ (fromJust newLastChar)])
 
 
 
@@ -56,4 +58,5 @@ generate :: Int -> String -> IO ()
 generate order alphabet = do
     -- putStrLn (db order alphabet "000" ["000"])
     -- print (db "000" ["000"])
-    putStrLn (db "000" ["000"])
+    let init = replicate order (alphabet !! 0)
+    putStrLn (db order alphabet init [init])

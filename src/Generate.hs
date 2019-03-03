49fcbd007ea45d7501d8575f9476b06709976999
alexis <alexis.auriac@epitech.eu>
2019-03-03 15:22:43 +0100

    generation: finished prefer one algo

--- src/Generate.hs
+++ src/Generate.hs
@@ -4,59 +4,24 @@ module Generate (
 
 import Data.Maybe
 
--- tryAddSeq :: String -> [String] -> String -> Maybe String
--- tryAddSeq [] _ _ = Nothing
--- tryAddSeq alphabet done lastN =
---     let a = take 1 alphabet in
---     if (lastN ++ a) `elem` done
---         then tryAddSeq (drop 1 alphabet) done lastN
---         else Just a
-
--- db :: Int -> String -> String -> [String] -> String
--- db order alphabet dBSeq done = do
---     let lenSeq = length dBSeq
---     let lastN = drop (lenSeq - order + 1) dBSeq
---     let newLastChar = tryAddSeq alphabet done lastN
-
---     case newLastChar of
---         Nothing -> dBSeq
---         _ -> db order alphabet (dBSeq ++ (fromJust newLastChar)) (done ++ [lastN ++ (fromJust newLastChar)])
-
--- tryAddSeq :: String -> [String] -> String -> Maybe String
--- tryAddSeq [] _ _ = Nothing
--- tryAddSeq alphabet done lastN =
---     let a = take 1 alphabet in
---     if (lastN ++ a) `elem` done
---         then tryAddSeq (drop 1 alphabet) done lastN
---         else Just a
-
-tryAddSeq :: String -> [String] -> String -> Maybe String
-tryAddSeq alphabet done lastN =
-    if (lastN ++ [alphabet !! 1]) `elem` done
-        then if (lastN ++ (take 1 alphabet)) `elem` done
-            then Nothing
-            else Just (take 1 alphabet)
-        else Just [(alphabet !! 1)]
+tryAddSeq :: String -> [String] -> String -> Int -> String
+tryAddSeq alphabet done lastN i
+    | i == length alphabet = take 1 alphabet
+    | otherwise = if (lastN ++ [alphabet !! i]) `elem` done
+        then tryAddSeq alphabet done lastN (i + 1)
+        else [(alphabet !! i)]
 
 db :: Int -> String -> String -> [String] -> String
--- db :: String -> [String] -> [String]
 db order alpha dBSeq done = do
     let lenSeq = length dBSeq
     let lastN = drop (lenSeq - order + 1) dBSeq
-    let newLastChar = tryAddSeq alpha done lastN
+    let newLastChar = tryAddSeq alpha done lastN 0
 
-    if length done == (length alpha) ^ order - 1
+    if length done == (length alpha) ^ order - order + 1
         then dBSeq
-        else db order alpha (dBSeq ++ (fromJust newLastChar)) (done ++ [lastN ++ (fromJust newLastChar)])
-    -- case newLastChar of
-        -- Nothing -> dBSeq
-        -- Nothing -> done
-
-
+        else db order alpha (dBSeq ++ newLastChar) (done ++ [lastN ++ newLastChar])
 
 generate :: Int -> String -> IO ()
 generate order alphabet = do
-    -- putStrLn (db order alphabet "000" ["000"])
-    -- print (db "000" ["000"])
-    let init = replicate order (alphabet !! 0)
+    let init = replicate order (alphabet !! ((length alphabet) - 1))
     putStrLn (db order alphabet init [init])

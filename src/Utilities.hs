acc4074e2b949c46aafb07273fd94078b9bb8c13
alexis <alexis.auriac@epitech.eu>
2019-04-10 21:41:43 +0200

    Added utility function that finds combinations
    It finds all the combinations given an order and an alphabet.

--- src/Utilities.hs
+++ src/Utilities.hs
@@ -1,7 +1,8 @@
 module Utilities (
         allDifferent,
         allElemsOf,
-        rotate
+        rotate,
+        combinations
     ) where
 
 allDifferent :: (Eq a) => [a] -> Bool
@@ -14,3 +15,9 @@ allElemsOf (c:xs) src = c `elem` src && (allElemsOf xs src)
 
 rotate :: (Eq a) => [a] -> [a]
 rotate (x:xs) = xs ++ [x]
+
+-- unused, finds all the combinations given an order and an alphabet
+combinations :: (Eq a) => Word -> [a] -> [[a]]
+combinations 0 _ = []
+combinations 1 alpha = [[i] | i <- alpha]
+combinations order alpha = [i:j | i <- alpha, j <- combinations (order - 1) alpha]
